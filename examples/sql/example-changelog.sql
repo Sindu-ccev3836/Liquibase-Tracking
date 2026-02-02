259531c0238a8aec77553ac6b02b616f047b4d41
Sindhuja <ccev3836@partner.nus.edu.sg>
2026-02-02 14:32:42 +0800

    Initial commit

---
+++ examples/sql/example-changelog.sql
@@ -0,0 +1,6 @@
+CREATE TABLE dbo.users (
+    id INT,
+    username VARCHAR(50),
+    email VARCHAR(100),
+    created_at DATETIME
+)
---
+++ schema/example-changelog.sql
@@ -0,0 +1,5 @@
+CREATE TABLE dbo.users (
+    id INT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100)
+)
